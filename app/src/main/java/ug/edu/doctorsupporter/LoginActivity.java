90cc23144bb4b9f0e1266859aafdae71e3b88eba
Bartek3 <pl.kasspee@gmail.com>
2017-10-28 00:44:05 +0200

    MainActivity 0.1

--- app/src/main/java/ug/edu/doctorsupporter/LoginActivity.java
+++ app/src/main/java/ug/edu/doctorsupporter/LoginActivity.java
@@ -1,10 +1,12 @@
 package ug.edu.doctorsupporter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +14,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    private static void zaloguj(View view){
-
+    public void login(View v) {
+        // Tutaj dodać sprawdzanie, czy ID i hasło się zgadzają
+        openMainActivity();
     }
+
+    public void openMainActivity() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+    }
+
+/* Metoda wywoływana przy błędnej próbie zalogowania, jeszcze nieaktywna
+    public void error(){
+        TextView errorText = findViewById(R.id.wrongLoginText);
+        errorText.setVisibility(View.VISIBLE);
+    } */
 
 }
 
