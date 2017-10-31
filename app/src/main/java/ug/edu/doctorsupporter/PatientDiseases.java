d175baac5e573f25a34cf4a5dee3a17076e63a0f
Bartek3 <pl.kasspee@gmail.com>
2017-10-31 20:08:53 +0100

    MainActivity 0.5, TODO: Patient Fragments

---
+++ app/src/main/java/ug/edu/doctorsupporter/PatientDiseases.java
@@ -0,0 +1,24 @@
+package ug.edu.doctorsupporter;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PatientDiseases extends Fragment {
+
+
+    public PatientDiseases() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_patient_diseases, container, false);
+    }
+
+}
---
+++ app/src/main/java/ug/edu/doctorsupporter/PatientExaminations.java
@@ -0,0 +1,24 @@
+package ug.edu.doctorsupporter;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PatientExaminations extends Fragment {
+
+
+    public PatientExaminations() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_patient_examinations, container, false);
+    }
+
+}
